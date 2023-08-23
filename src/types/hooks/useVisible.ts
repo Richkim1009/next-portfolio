4f03a9dfbd2c31058e02c73030fd7f914d6da09d
polaris1009 <gerrad1009@daum.net>
2023-08-23 21:13:18 +0900

    useVisible custom hook

---
+++ src/types/hooks/useVisible.ts
@@ -0,0 +1,1 @@
+export type UseVisibleReturn = [boolean, React.RefObject<HTMLDivElement>];
---
+++ src/utils/customHook/useVisible.tsx
@@ -0,0 +1,35 @@
+import { UseVisibleReturn } from "@/types/hooks/useVisible";
+import { useEffect, useRef, useState } from "react";
+
+export function useVisible(): UseVisibleReturn {
+  const [isVisible, setIsVisible] = useState(false);
+  const elementRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+        } else {
+          setIsVisible(false);
+        }
+      },
+      {
+        threshold: 0.5
+      }
+    );
+
+    if (elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+
+    return () => {
+      if (elementRef.current) {
+        observer.unobserve(elementRef.current);
+      }
+    };
+  }, []);
+
+  return [isVisible, elementRef]
+}
